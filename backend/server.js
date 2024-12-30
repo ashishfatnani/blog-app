92424222eaf4c33ef038339e2ae7a0ba24287cec
Ashish Fatnani <ashishfatnani3@gmail.com>
2024-12-30 18:15:35 -0500

    feat: http server configured (done)

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const http = require("http");
+const connectDB = require("./config/db");
+
+// Initialize app and connect to DB
+const app = express();
+connectDB();
+
+// Middleware
+app.use(cors());
+app.use(bodyParser.json());
+
+// Start HTTP server
+const PORT = 5000;
+const server = http.createServer(app);
+server.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
